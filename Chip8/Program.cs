5a8e1a3f20beed03fdd0ace7a799015543ae1b37
Italo Di Renzo <i.direnzo@vehikl.com>
2019-09-18 23:52:10 -0400

    Reading in opcodes from sample file

--- Chip8/Program.cs
+++ Chip8/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 
 namespace Chip8
 {
@@ -6,7 +7,21 @@ namespace Chip8
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            using (var rom = File.OpenRead("roms/IBM Logo.ch8"))
+            {
+                Console.WriteLine($"File is {rom.Length} bytes long");
+
+                using (var reader = new BinaryReader(rom))
+                {
+                    while (rom.Position < rom.Length)
+                    {
+                        // CHIP-8 opcodes are 2 bytes long and stored in big-endian.
+                        var opcode = (ushort)((reader.ReadByte() << 8) | reader.ReadByte());
+                        Console.WriteLine($"OPCODE is {opcode:X4}.");
+                    }
+                    Console.WriteLine("\nEnd of ROM file.");
+                }// using BinaryReader
+            }// using FileStream
         }
     }
 }
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Chip8
 CHIP-8 interpreter
+
+Hardware documentation on CHIP-8 can be found [on Wikipedia](https://en.wikipedia.org/wiki/CHIP-8).
